7651eb00a47ef1ed81557ea37baec06b9dc3f0ae
dima-starkov <62872531+dima-starkov@users.noreply.github.com>
2021-09-19 17:13:43 +0300

    commit

---
+++ BondHomework/ViewControllerB.swift
@@ -0,0 +1,29 @@
+//
+//  ViewControllerB.swift
+//  BondHomework
+//
+//  Created by Дмитрий Старков on 19.09.2021.
+//
+
+import UIKit
+import ReactiveKit
+import Bond
+
+class ViewControllerB: UIViewController {
+
+    @IBOutlet weak var textField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        textField.reactive.text
+            .delay(interval: 2.0)
+            .map {"отправка запроса для \($0 ?? "")"}
+            .observeNext{ print($0)}
+
+    }
+    
+
+    
+
+}
