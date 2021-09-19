75588f96449cd09cf1979ea9658060c1dca7fd61
dima-starkov <62872531+dima-starkov@users.noreply.github.com>
2021-09-19 19:30:32 +0300

    find in table

--- BondHomework/ViewControllerС.swift
+++ BondHomework/ViewControllerС.swift
@@ -15,11 +15,28 @@ class ViewControllerCCC: UIViewController {
     @IBOutlet weak var deleteButton: UIButton!
     @IBOutlet weak var table: UITableView!
     
+    @IBOutlet weak var findTextField: UITextField!
+    
     let array = ["Дима","Антон","Виктор","Денис","Никита","Сергей","Михаил","Андрей"]
-    var tableArray = Property(["ll"])
+    var tableArray = Property(["Fy"])
+    var findArray = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        findTextField.reactive.text
+            .throttle(for: 2.0)
+            .compactMap{ [unowned self] text in
+                
+                for element in self.tableArray.value {
+                    if element == text {
+                        self.findArray.append(element)
+                    }
+            }
+                self.tableArray.value.removeAll()
+                self.tableArray.value = self.findArray
+            }
+        
         
         addButton.reactive.tap
             .observeNext{ [unowned self] in self.tableArray.value.append(self.array.randomElement()!)}
