0a8f1ed4f6ae6748c96455e900bd896acab181a5
dima-starkov <62872531+dima-starkov@users.noreply.github.com>
2021-09-29 20:07:09 +0300

    find fix

--- BondHomework/ViewControllerС.swift
+++ BondHomework/ViewControllerС.swift
@@ -19,30 +19,40 @@ class ViewControllerCCC: UIViewController {
     
     let array = ["Дима","Антон","Виктор","Денис","Никита","Сергей","Михаил","Андрей"]
     var tableArray = Property(["None"])
+    var arrayB = [String]()
     var findArray = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         findTextField.reactive.text
-            .debounce(for: 2.0)
+            //.debounce(for: 2.0)
             .observeNext{ [unowned self] text in
-                
+                guard let text = text else { return }
+                if text != "" {
                 for element in self.tableArray.value {
-                    if element == text {
+                    if element.hasPrefix(text) {
                         self.findArray.append(element)
                     }
             }
-                self.tableArray.value.removeAll()
-                self.tableArray.value = self.findArray
+                    self.tableArray.value.removeAll()
+                    self.tableArray.value = findArray
+                } else {
+                    tableArray.value.removeAll()
+                    tableArray.value = arrayB
+                }
             }
         
         
         addButton.reactive.tap
-            .observeNext{ [unowned self] in self.tableArray.value.insert(array.randomElement()!, at: 0)}
+            .observeNext{ [unowned self] in self.tableArray.value.insert(array.randomElement()!, at: 0)
+                arrayB = tableArray.value
+            }
             
         deleteButton.reactive.tap
-            .observeNext{[unowned self] in self.tableArray.value.removeLast()}
+            .observeNext{[unowned self] in self.tableArray.value.removeLast()
+                arrayB = tableArray.value
+            }
         
         tableArray.bind(to: table) { data, indexPaht, table in
             guard let cell = table.dequeueReusableCell(withIdentifier: "cell") else { return UITableViewCell() }
